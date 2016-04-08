e3bf365eff0e57aa5f192410ac6a0280ef0d2b7c
Rasmus Hjulskov <rhjulskob@gmail.com>
2016-04-08 14:46:41 +0200

    0.0.3 version number

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'nordlys:winston-logstash',
-  version: '0.0.2',
+  version: '0.0.3',
   // Brief, one-line summary of the package.
   summary: 'Client to server winston logging combined with Logstash transport',
   // URL to the Git repository containing the source code for this package.
